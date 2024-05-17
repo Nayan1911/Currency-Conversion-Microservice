b8bba60542e1ec15c1e1c51a3457ba355b805a02
Nayan1911 <sharmanayan39@gmail.com>
2024-05-18 02:38:29 +0530

    Terraform GKE

--- 01-currency-exchange-microservice-basic/Configuration/main.tf
+++ 01-currency-exchange-microservice-basic/Configuration/main.tf
@@ -8,12 +8,8 @@ resource "google_container_cluster" "cluster" {
   location = var.zone
 
   initial_node_count = 1  # Replaces min_count
-  node_pool_defaults {  # Replaces node_pool block
-    machine_type = "n1-standard-1"
-    disk_size_gb  = 100
-    disk_type    = "pd-standard"
-  }
 }
+
 
 output "cluster_endpoint" {
   value = google_container_cluster.cluster.endpoint
