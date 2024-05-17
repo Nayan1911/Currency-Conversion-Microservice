f11d0a95d20cc0d5812590783b278daa53af6023
Nayan1911 <sharmanayan39@gmail.com>
2024-05-18 02:20:21 +0530

    Terraform GKE

--- 01-currency-exchange-microservice-basic/Configuration/deployment.tf
+++ 01-currency-exchange-microservice-basic/Configuration/deployment.tf
@@ -27,32 +27,38 @@ resource "kubernetes_deployment" "currency_exchange" {
         container {
           name  = "currency-exchange"
           image = "sharmanayan/currency-exchange:0.0.1-RELEASE"
-          ports {
+
+          # Use "port" argument instead of a block
+          port {
             container_port = 8000
           }
+
+          # Use "resources" argument with "requests" and "limits" as nested arguments
           resources {
-            requests {
+            requests = {
               cpu    = "100m"
               memory = "512Mi"
             }
-            limits {
+            limits = {
               cpu    = "500m"
               memory = "1024Mi"
             }
           }
+
           readiness_probe {
             http_get {
               path = "/"
-              port = "liveness-port"
+              port = 8000
             }
             initial_delay_seconds = 60
             period_seconds        = 10
             failure_threshold     = 5
           }
+
           liveness_probe {
             http_get {
               path = "/"
-              port = "liveness-port"
+              port = 8000
             }
             initial_delay_seconds = 60
             period_seconds        = 10
--- 01-currency-exchange-microservice-basic/Configuration/main.tf
+++ 01-currency-exchange-microservice-basic/Configuration/main.tf
@@ -4,19 +4,18 @@ provider "google" {
 }
 
 resource "google_container_cluster" "cluster" {
-  name     = var.cluster_name
+  name    = var.cluster_name
   location = var.zone
 
-  node_pool {
-    name       = "default-pool"
+  initial_node_count = 1  # Replaces min_count
+  node_pool_default {  # Replaces node_pool block
     machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    disk_type = "pd-standard"
-    min_count = 1
-    max_count = 3
+    disk_size_gb  = 100
+    disk_type    = "pd-standard"
   }
 }
 
 output "cluster_endpoint" {
   value = google_container_cluster.cluster.endpoint
 }
+
